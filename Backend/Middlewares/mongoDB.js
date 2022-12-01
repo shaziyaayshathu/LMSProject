9d94d6a9c925084423e7b0cd488c51f4b1763f9a
Anjal-Rajeev <anjalrajeev5@gmail.com>
2022-12-01 11:06:24 +0530

    login sample validation added

--- Backend/Middlewares/mongoDB.js
+++ Backend/Middlewares/mongoDB.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+
 mongoose.connect('mongodb+srv://AnjalRajeev:anjalrajeev@cluster0.cgp2vwd.mongodb.net/LMS_Database?retryWrites=true&w=majority',{ 
     useNewUrlParser:true,
     useUnifiedTopology:true
